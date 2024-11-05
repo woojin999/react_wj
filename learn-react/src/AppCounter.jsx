33b1845f80774c2fd50cd50b4fc0d75f364ca9bf
LEE WOOJIN <24864@naver.com>
2024-11-05 13:18:54 +0900

    useState

--- learn-react/src/AppCounter.jsx
+++ learn-react/src/AppCounter.jsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
-import "./App.css";
+import "./AppCounter.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 
 export default function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
--- learn-react/src/components/Counter.jsx
+++ learn-react/src/components/Counter.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
+
 // 함수 선언식
-export default function Counter() {
-  return <button>Counter</button>;
+export default function Counter({onTotal}) {
+ const [counter,setCounter] = useState(0);
+
+ const handleCounter = () =>{
+  setCounter(counter + 1);
+  onTotal();
+ }
+ // 상태,로직
+  return <button onClick={handleCounter}>Counter : {counter}</button>;
 }
 
 // 함수 표현식
