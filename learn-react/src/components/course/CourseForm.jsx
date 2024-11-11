13effdbe05a7bda799d9dde064c8a4139f6500a8
LEE WOOJIN <24864@naver.com>
2024-11-11 15:50:30 +0900

    중첩 객체 업데이트

--- learn-react/src/components/course/CourseForm.jsx
+++ learn-react/src/components/course/CourseForm.jsx
@@ -1,29 +1,48 @@
-import { useState } from "react";
+// import { useState } from "react";
+import { useImmer } from "use-immer";
 import Card from "../Card";
 
 export default function CourseForm(params) {
-  const [form, setForm] = useState({
+  const [form, updateForm] = useImmer({
     title: "리액트 강의",
     description: "리액트 기초부터 실천까지",
+    info: {
+      level: 1,
+      skill: "React",
+    },
   });
   function handleCourseForm(e) {
     e.preventDefault();
   }
   const handleChange = (e) => {
-    console.log('e.target.name:', e.target.name);
-    
-    setForm({
-      ...form,
-      [e.target.name]: e.target.value,
+    updateForm((draft) => {
+      draft[e.target.name] = e.target.value;
+    });
+
+    // setForm({
+    //   ...form,
+    //   [e.target.name]: e.target.value,
+    // });
+  };
+
+  const handleSkillChange = (e) => {
+    updateForm((draft) => {
+      draft.info.skill = e.target.value;
+    });
+    // setForm({
+    //   ...form,
+    //   info: {
+    //     ...form.info,
+    //     skill: e.target.value,
+    //   },
+    // });
+  };
+  const handleLevelChange = (e) => {
+    updateForm((draft) => {
+      draft.info.level = e.target.value;
     });
   };
 
-  // const handleDescriptionChange = (e) => {
-  //   setForm({
-  //     ...form,
-  //     description: e.target.value,
-  //   });
-  // };
   return (
     <Card title="강의등록">
       <form
@@ -44,6 +63,33 @@ export default function CourseForm(params) {
           value={form.description}
           onChange={handleChange}
         />
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <label htmlFor="skill" style={{ width: "100px" }}>
+            스킬
+          </label>
+          <input
+            type="text"
+            name="skill"
+            id="skill"
+            value={form.info.skill}
+            onChange={handleSkillChange}
+          />
+        </div>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <label htmlFor="level" style={{ width: "100px" }}>
+            난이도
+          </label>
+          <select
+            name="level"
+            id="level"
+            value={form.info.level}
+            onChange={handleLevelChange}
+          >
+            <option value="0">입문</option>
+            <option value="1">초급</option>
+            <option value="2">중급</option>
+          </select>
+        </div>
         <input type="submit" value="등록" />
         {(form.title || form.description) && (
           <div
@@ -56,6 +102,8 @@ export default function CourseForm(params) {
           >
             {form.title && <p>제목 - {form.title}</p>}
             {form.description && <p>설명 - {form.description}</p>}
+            {form.info.skill && <p>스킬 - {form.info.skill}</p>}
+            {form.info.level && <p>난이도 - {form.info.level}</p>}
           </div>
         )}
       </form>
