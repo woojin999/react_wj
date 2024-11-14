d1e2386123ef3daa021d849368b2648658238912
LEE WOOJIN <24864@naver.com>
2024-11-14 13:21:36 +0900

    Immer

--- learn-react/src/AppTodo.jsx
+++ learn-react/src/AppTodo.jsx
@@ -2,11 +2,12 @@ import React, { useReducer, useState } from "react";
 import "./App.css";
 import TodoList from "./components/todo/TodoList";
 import todoReducer from "./reducer/todo-reducer";
+import {useImmerReducer} from 'use-immer';
 
 function AppTodo(props) {
   const [todoText, setTodoText] = useState("");
 
-  const [todos, dispatch] = useReducer(todoReducer, [
+  const [todos, dispatch] = useImmerReducer(todoReducer, [
     { id: 0, text: "HTML&CSS 공부하기", done: false },
     { id: 1, text: "자바스크립트 공부하기", done: false },
   ]);
--- learn-react/src/reducer/todo-reducer.js
+++ learn-react/src/reducer/todo-reducer.js
@@ -1,36 +1,43 @@
-export default function todoReducer(todos, action) {
+export default function todoReducer(draft, action) {
   // type
   switch (action.type) {
     case "added": {
       const { nextId, todoText } = action;
-      return [...todos, { id: nextId, text: todoText }];
+      draft.push({ id: nextId, text: todoText })
+      break;
     }
     case "added_index": {
       const { insertAt, nextId, todoText } = action;
-      return [
-        // 삽입 지점 이전항목
-        ...todos.slice(0, insertAt),
-        { id: nextId, text: todoText, done: false },
+      draft.splice(insertAt, 0, { id: nextId, text: todoText, done: false });
+      break;
+      // return [
+      //   // 삽입 지점 이전항목
+      //   ...todos.slice(0, insertAt),
+      //   { id: nextId, text: todoText, done: false },
 
-        // 삽입 지점 이후 항목
-        ...todos.slice(insertAt),
-      ];
+      //   // 삽입 지점 이후 항목
+      //   ...todos.slice(insertAt),
+      // ];
     }
     case "deleted": {
       const { deleteId } = action;
-      return todos.filter((item) => item.id !== deleteId);
+      
+      return draft.filter((item) => item.id !== deleteId);
     }
     case "done": {
       const { id, done } = action;
-      return todos.map((item) => {
-        if (item.id == id) {
-          return { ...item, done };
-        }
-        return item;
-      });
+      const target = draft.find(item => item.id ===id);
+      target.done= done;
+      // return todos.map((item) => {
+      //   if (item.id == id) {
+      //     return { ...item, done };
+      //   }
+      //   return item;
+      // });
+      break;
     }
     case "reverse": {
-      return todos.toReversed();
+      return draft.toReversed();
     }
 
     default: {
