9d84a298e55bc6e739d94a800f1b2c119a2ab345
LEE WOOJIN <24864@naver.com>
2024-11-15 13:44:41 +0900

    useEffect

---
+++ learn-react/src/AppEffect.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+
+function Courses() {
+  const [list, setList] = useState([]);
+  const [filter, setFilter] = useState("all");
+
+  useEffect(() => {
+    fetch(`data/courses_${filter}.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("데이터 조회 성공");
+        setList(data);
+      });
+    return () => {
+      console.log("연결 해제");
+    };
+  }, [filter]);
+  return (
+    <>
+      <label htmlFor="all">전체</label>
+      <input
+        id="all"
+        type="radio"
+        value="all"
+        checked={filter === "all"}
+        onChange={(e) => setFilter(e.target.value)}
+      />
+      <label htmlFor="favorite">좋아요</label>
+      <input
+        id="favorite"
+        type="radio"
+        value="favorite"
+        checked={filter === "favorite"}
+        onChange={(e) => setFilter(e.target.value)}
+      />
+      <ul>
+        {list.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+function AppEffect() {
+  // dom 조작하기
+  // useEffect(()=>{
+  //   const h2 = document.querySelector('#title');
+  //   h2.textContent = 'data fetching';
+  // },[])
+
+  
+
+  const [show,setShow] = useState(true);
+
+  return (
+    <>
+      <h2 id="title">데이터 가져오기</h2>
+      <button onClick={()=> setShow(!show)}>toggle</button>
+      <hr />
+      {show && <Courses/>}
+    </>
+  );
+}
+
+export default AppEffect;
--- learn-react/src/main.jsx
+++ learn-react/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './AppTodo.jsx'
+import App from './AppEffect.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
